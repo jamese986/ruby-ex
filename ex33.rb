f3adea418182f5d40955219921067e0993e0753e
James English <james.top986@gmail.com>
2018-08-06 11:38:43 +0100

    for loops arrays printing specific element of an array using the range method 2 dot range method 3 dot range method (excludes the last value)

--- ex33.rb
+++ ex33.rb
@@ -1,6 +1,12 @@
+# Initialising a counter called i
+# Initialising an empty array called number
 i = 0
 numbers = []
 
+# the while loop counts through 0 - 6
+# each time the loops goes round it adds 1 to the counter
+# numbers.push(i) pushes the counter to the end of the stack / array
+# and prints on a new line
 while i < 6
   puts "At the top i is #{i}"
   numbers.push(i)
@@ -10,6 +16,7 @@ while i < 6
   puts "At the bottom i is #{i}"
 end
 
+# prints the numbers out on a new line
 puts "The numbers: "
 
 # remember you can write this 2 other ways?
