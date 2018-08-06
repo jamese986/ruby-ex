14e3db1e566a59693bc06b1f40c348604f920726
James English <james.top986@gmail.com>
2018-08-06 13:16:27 +0100

    Exercise 34: array / counting within an array

---
+++ ex34.rb
@@ -0,0 +1,11 @@
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1. = ruby
+# The third (3rd) animal. = kangaroo
+# The first (1st) animal. = bear
+# The animal at 3. peacock
+# The fifth (5th) animal. platypus
+# The animal at 2. ruby
+# The sixth (6th) animal.
+# The animal at 4.
